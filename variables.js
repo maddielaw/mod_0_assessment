7591a372dcf4fd47849688c192ac2ece034eba88
Madison Law <madisonlaw1@gmail.com>
2021-10-21 10:36:03 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,11 @@
+var famousPerson = "Ryan Reynolds";
+var isActive = true;
+famousPerson = "Black Lively";
+
+var num3 = 5;
+var num9 = 10;
+var sum = num3 + num9;
+
+var animals = ["Zebra", "Lion", "Mongoose"];
+animals.shift();
+console.log(famousPerson.length);
