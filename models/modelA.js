50e39733a629c7c9c12b7bc076815baab79fa5f5
Madison Law <madisonlaw1@gmail.com>
2021-10-21 10:44:08 -0600

    Add airport model to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: "Denver International Airport",
+  location: "Denver, Colorado",
+  numTerminals: 1,
+  numRunways: 6,
+  hasInternationalFlights: true,
+  isBiggestInWorld: false,
+  restaurants: ["Root Down", "Elway's", "Aviator's"],
+  shopping: ["Bearfruit Jewelry", "See's Candies", "Misura"]
+};
