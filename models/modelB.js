5e491bdd57913ff8327e76dd877a6d3870c39bad
Madison Law <madisonlaw1@gmail.com>
2021-10-21 10:49:57 -0600

    Add vehicle model to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var myVehicle = {
+  make: "Ford",
+  model: "Fiesta",
+  milesToGallon: 27,
+  year: 2016,
+  hasFourWheelDrive: false,
+  isFourDoor: true,
+  features: ["heated mirrors", "bluetooth connectivity", "keyless ignition"],
+  tripsTaken: ["Alaska Highway", "Pacific Highway 101", "US Route 1"]
+};
